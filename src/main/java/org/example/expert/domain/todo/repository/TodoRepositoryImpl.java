38fc43a9706bb21dd52a2e75abbd749516210be6
doldollee00 <doldollee00@gmail.com>
2025-09-15 11:46:45 +0900

    refactor: QueryDSL 작성

---
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepositoryImpl.java
@@ -0,0 +1,28 @@
+package org.example.expert.domain.todo.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+import org.example.expert.domain.todo.entity.Todo;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+import static org.example.expert.domain.todo.entity.QTodo.todo;
+import static org.example.expert.domain.user.entity.QUser.user;
+
+@Repository
+@RequiredArgsConstructor
+public class TodoRepositoryImpl implements TodoRepositoryCustom{
+
+    private final JPAQueryFactory jpaQueryFactory;
+
+    @Override
+    public Optional<Todo> findByIdWithUser(Long todoId) {
+        Todo result = jpaQueryFactory
+                .selectFrom(todo)
+                .join(todo.user, user).fetchJoin()
+                .where(todo.id.eq(todoId))
+                .fetchOne();
+        return Optional.ofNullable(result);
+    }
+}
