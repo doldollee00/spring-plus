953288bba2ea63065045959da5dc62b9fc4ce279
doldollee00 <doldollee00@gmail.com>
2025-09-18 15:23:20 +0900

    test: EC2 테스트 시작2

--- src/main/java/org/example/expert/config/HealthCheck.java
+++ src/main/java/org/example/expert/config/HealthCheck.java
@@ -9,7 +9,7 @@ public class HealthCheck {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
-        return ResponseEntity.ok("OKss");
+        return ResponseEntity.ok("OK");
     }
 
 }
